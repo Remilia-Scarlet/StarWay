6b316acf4e604df00663b1407707c091ef207ecf
dango <dangochan@qq.com>
2016-09-29 00:46:19 +0800

    Fix compile bug

--- Engine/Math/precomp.h
+++ Engine/Math/precomp.h
@@ -1,4 +1,4 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
 #include "Settings.h"
-#include "TinyEngine/TinyEngine.h"
+#include "TinyEngine/Engine/EngineDefs.h"
--- Engine/Math/vector/Vector.h
+++ Engine/Math/vector/Vector.h
@@ -4,24 +4,57 @@ class Vector2 : public MathStorage<float, 2>
 {
 public:
 	inline Vector2() {}
-	inline Vector2(float x, float y);
+	inline Vector2(float x, float y)
+	{
+		setValue(0, x);
+		setValue(1, y);
+	}
 	~Vector2() {}
-	inline Vector2& operator=(const float& value);
+	inline Vector2& operator=(const float& value)
+	{
+		setValue(0, value);
+		setValue(1, value);
+		return *this;
+	}
 };
 
 class Vector3 : public MathStorage<float, 3>
 {
 public:
 	inline Vector3() {}
-	inline Vector3(float x, float y, float z);
+	inline Vector3(float x, float y, float z)
+	{
+		setValue(0, x);
+		setValue(1, y);
+		setValue(2, z);
+	}
 	~Vector3() {}
-	inline Vector3& operator=(const float& value);
+	inline Vector3& operator=(const float& value)
+	{
+		setValue(0, value);
+		setValue(1, value);
+		setValue(2, value);
+		return *this;
+	}
 };
 class Vector4 : public MathStorage<float, 4>
 {
 public:
 	inline Vector4() {}
-	inline Vector4(float x, float y, float z, float w);
+	inline Vector4(float x, float y, float z, float w)
+	{
+		setValue(0, x);
+		setValue(1, y);
+		setValue(2, z);
+		setValue(3, w);
+	}
 	~Vector4() {}
-	inline Vector4& operator=(const float& value);
+	inline Vector4& operator=(const float& value)
+	{
+		setValue(0, value);
+		setValue(1, value);
+		setValue(2, value);
+		setValue(3, value);
+		return *this;
+	}
 };
