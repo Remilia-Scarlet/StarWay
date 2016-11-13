63f6a3063d4a286678fe4c0386fc1f551848d1b1
dangogo <dangochan@qq.com>
2016-11-13 10:13:41 +0800

    nurbs

--- StarWay/Script/spline/nurbs.lua
+++ StarWay/Script/spline/nurbs.lua
@@ -9,11 +9,33 @@ function Nurbs:create()
 	return instance
 end
 
+function Nurbs:get(u)
+	local a = {}
+	local b = 0
+	for i = 1,#self.P do
+		a = a + self.w[i]* self:N[i,k,u] * self.P[i] 
+	end
+	for i = 1,#self.P do
+		b = b + self.w[i] * self:N[i,k,u]
+	end
+	return a / b
+end
+
 function Nurbs:N(i,n,u)
-	return f(i,n,u) * N(i,n-1,u) + g(i+1,n,u) * N(i+1,n-1,u)
+	if(n == 0) then
+		if(k[i] <= u and u < k[i + i]) then
+			return 1
+		else
+			return 0
+		end
+	end
+	return self:f(i,n,u) * self:N(i,n-1,u) + self:g(i+1,n,u) * self:N(i+1,n-1,u)
 end
 
 function Nurbs:f(i,n,u)
-	return (u - k[i]) / (k[i+n] - k[i])
+	return (u - self:k[i]) / (self:k[i+n] - self:k[i])
 end
 
+function Nurbs:g(i,n,u)
+	return (self:k[i+n] - u) / (self:k[i+n] - self:k[i])
+end
