cf01ba556326f4fea17966427ca98ec00799d459
dango <dangochan@qq.com>
2017-12-29 23:05:11 +0800

    Add lua proj

--- Tools/luac/src/GenerateProjects.bat
+++ Tools/luac/src/GenerateProjects.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd "%CD%"
 cd  /d "%~dp0%"
-call ..\..\Sharpmake\build\Sharpmake.Application.exe /sources("luac.sharpmake")
+call ..\..\Sharpmake\build\Sharpmake.Application.exe /sources("lua.sharpmake")
 popd
 pause
